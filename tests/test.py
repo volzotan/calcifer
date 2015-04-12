7893b8600e056f52d59aa57c1c8a40f6746533bd
Christopher Getschmann <christopher@volzo.de>
2015-04-12 22:51:02 +0200

    more unittests

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from calcifer.util import *
+from calcifer.mainframe import *
 
 class TestMessage(unittest.TestCase):
 
@@ -31,10 +32,63 @@ class TestBackstore(unittest.TestCase):
         pass
 
     def test_add(self):
-        self.backstore.add(Message("testpayload", ))
-        self.assertEqual('foo'.upper(), 'FOO')
+        msg1 = Message("testpayload1")
+        msg2 = Message("testpayload2")
 
+        self.backstore.add(msg1)
+        self.backstore.add(msg2)
+
+        self.assertEqual(len(self.backstore.get_all()), 2)
+        self.assertEqual(self.backstore.get(msg1), msg1)
+
+    def test_cleanup(self):
+        pass
+
+
+class TestMainframe(unittest.TestCase):
+
+    def setUp(self):
+        __package__ == "calcifer"
+
+    def tearDown(self):
+        self.mainframe.shutdown()
+
+    def test_params(self):
+        params = {
+            "debug": True,
+            "logging_level": logging.WARN
+        }
+
+        self.mainframe = Mainframe(params)
+
+        self.assertEqual(self.mainframe.debug, True)
+        self.assertEqual(self.mainframe.logging_level, logging.WARN)
+        # self.assertEqual(self.mainframe.logger,)
+
+class TestSocketCommunication(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testStart(self):
+        calcifer.start()
+
+
+class TestPlugins(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    #test every plugin with every conf in ../calcifer/plugins
+
+
+
+# if __name__ == '__main__':
+#     suite = unittest.TestLoader().loadTestsFromTestCase(TestBackstore)
+#     unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestBackstore)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
